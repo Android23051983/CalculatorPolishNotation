77b3d4cfad597d1db09b4a80412b652666f806d3
Andrei <ikar2008@hotmail.com>
2023-03-13 15:48:32 +0300

    Добавьте файлы проекта.

---
+++ CalculatorPolishNotation/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+using CalculatorPolishNotation;
+
+while (true) {
+    Write("Введите выражение: ");
+    WriteLine(RPN.Calculate(ReadLine()));
+}
+
+
